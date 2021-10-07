dbfa116daa8786c1f9e24ce7ea8e2c97a9de6b64
jzaborowski <jakub.zaborowski@skriware.com>
2021-10-07 15:15:19 +0200

    Project structure changes vol. 2

--- scripts/materials_copy.sh
+++ scripts/materials_copy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # WORKS ONLY ON MAC OS STANDARD CURA INSTALLATION
 projectname=$(basename $PWD)
-if [[ $projectname != $"zortrax_cura_profiles" ]]
+if [[ $projectname != $"CuraProfiles_SK2" ]]
 then
-	echo -e "\033[0;33mMake sure you run the script from the project main folder."
+	echo -e "\033[0;33mMake sure you run the script from the project's scripts folder."
 fi
 echo -e "\033[0;32m->Copying material profile files\033[1;30m"
 cp -rv ../materials/* /Applications/Ultimaker\ Cura.app/Contents/Resources/resources/materials/
